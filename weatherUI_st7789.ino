d5e80885d5747ef999a2efda52845ef41643c818
Andreas Vrachimi <avrachimi@hotmail.com>
2024-09-10 22:04:52 +0100

    added sun, cloud and rain icons

--- weatherUI_st7789.ino
+++ weatherUI_st7789.ino
@@ -47,6 +47,8 @@ Adafruit_ST7789 tft = Adafruit_ST7789(TFT_CS, TFT_DC, TFT_RST);
 
 // COLOURS
 #define COLOR_SKY 0x96ff
+#define COLOR_CLOUD 0xa514
+#define COLOR_RAIN 0x30fd
 
 // SCREEN
 #define SCREEN_WIDTH 320
@@ -69,17 +71,18 @@ void setup(void) {
 
   tft.fillScreen(COLOR_SKY);
 
-  updateUI();
+  updateUI("sun");
 }
 
 void loop() {
 }
 
-void updateUI() {
+void updateUI(String type) {
+  tft.fillScreen(COLOR_SKY);
   // TODO: Fetch weather data
 
   // TODO: Draw main component
-  drawMainComponent();
+  drawMainComponent(type);
 
   // TODO: draw right component
   tft.drawFastVLine(SCREEN_WIDTH / 6 * 5, 0, SCREEN_HEIGHT / 5 * 3, ST77XX_BLACK);
@@ -88,42 +91,37 @@ void updateUI() {
   tft.drawFastHLine(0, SCREEN_HEIGHT / 5 * 3, SCREEN_WIDTH, ST77XX_BLACK);
 }
 
-void drawMainComponent() {
+void drawMainComponent(String type) {
   int x = 0;
   int y = 0;
 
   int componentWidth = SCREEN_WIDTH / 6 * 5;
   int componentHeight = SCREEN_HEIGHT / 5 * 3;
 
-  tft.drawFastVLine(componentWidth / 2, 0, componentHeight, ST77XX_RED);  // HELPER
+  //tft.drawFastVLine(componentWidth / 2, 0, componentHeight, ST77XX_RED);  // HELPER
 
   // TODO: create fn to draw sun/cloud/rain icons
   int iconComponentWidth = componentWidth / 2;
   int iconComponentHeight = componentHeight / 5 * 4;
 
-  tft.drawFastHLine(0, iconComponentHeight, iconComponentWidth, ST77XX_RED);  // HELPER
+  //tft.drawFastHLine(0, iconComponentHeight, iconComponentWidth, ST77XX_RED);  // HELPER
 
-  drawWeatherIcon(componentWidth, componentHeight);
+  drawWeatherIcon(componentWidth, componentHeight, type);
 
   drawWeatherLocationText(componentWidth, componentHeight, iconComponentHeight);
 
-  tft.drawFastHLine(iconComponentWidth, iconComponentHeight, iconComponentWidth, ST77XX_RED);  // HELPER
+  //tft.drawFastHLine(iconComponentWidth, iconComponentHeight, iconComponentWidth, ST77XX_RED);  // HELPER
 
-  int tempTextWidth = 70;
-  int tempTextHeight = 55;
+  drawWeatherTemp(13, iconComponentWidth, iconComponentHeight);
 
-  tft.setCursor(iconComponentWidth + iconComponentWidth / 2 - tempTextWidth / 2, iconComponentHeight / 2 - tempTextHeight / 2);
-  tft.setTextColor(ST77XX_BLACK);
-  tft.setTextWrap(false);
-  tft.setTextSize(7);
-  tft.print("17");
+  drawLowHighTemps(12, 26, componentWidth, componentHeight);
 }
 
-void drawWeatherIcon(int parentWidth, int parentHeight) {
+void drawWeatherIcon(int parentWidth, int parentHeight, String type) {
   int iconComponentWidth = parentWidth / 2;
   int iconComponentHeight = parentHeight / 5 * 4;
 
-  tft.drawFastHLine(0, iconComponentHeight, iconComponentWidth, ST77XX_RED);  // HELPER
+  //tft.drawFastHLine(0, iconComponentHeight, iconComponentWidth, ST77XX_RED);  // HELPER
 
   int iconPaddingX = iconComponentWidth * .1;
   int iconPaddingY = iconComponentHeight * 0.1;
@@ -131,7 +129,8 @@ void drawWeatherIcon(int parentWidth, int parentHeight) {
   int iconRadius = (iconComponentHeight - iconPaddingY) / 2;
   int iconX = iconComponentWidth / 2;
   int iconY = iconComponentHeight / 2;
-  tft.drawCircle(iconX, iconY, iconRadius, ST77XX_YELLOW);
+
+  drawIcon(type, iconX, iconY, iconRadius);
 }
 
 void drawWeatherLocationText(int parentWidth, int parentHeight, int iconComponentHeight) {
@@ -152,5 +151,134 @@ void drawWeatherLocationText(int parentWidth, int parentHeight, int iconComponen
   tft.print("London, UK");
 }
 
-void drawWeatherTemp() {
+void drawWeatherTemp(int temp, int parentWidth, int parentHeight) {
+  int tempTextWidth = 70;
+  int tempTextHeight = 55;
+
+  int tempStartX = parentWidth + parentWidth / 2 - tempTextWidth / 2 - 5;
+  int tempStartY = parentHeight / 2 - tempTextHeight / 2;
+  tft.setCursor(tempStartX, tempStartY);
+  tft.setTextColor(ST77XX_BLACK);
+  tft.setTextWrap(false);
+  tft.setTextSize(7);
+  tft.print(temp);
+
+  tft.fillCircle(tempStartX + tempTextWidth + 14, tempStartY + 2, 4, ST77XX_BLACK);
+  tft.fillCircle(tempStartX + tempTextWidth + 14, tempStartY + 2, 2, COLOR_SKY);
+}
+
+void drawLowHighTemps(int lowTemp, int highTemp, int parentWidth, int parentHeight) {
+  int iconComponentWidth = parentWidth / 2;
+  int iconComponentHeight = parentHeight / 5 * 4;
+
+  int paddingX = iconComponentWidth * .1;
+  int paddingY = iconComponentHeight * .1;
+
+  int textWidth = 24;
+
+  int lowStartX = iconComponentWidth + paddingX;
+  int highStartX = parentWidth - paddingX - textWidth;
+
+  int highLowHeight = parentHeight / 5;
+  int startY = iconComponentHeight + highLowHeight / 2 - 3;
+
+  tft.setCursor(lowStartX, startY);
+  tft.setTextColor(ST77XX_BLACK);
+  tft.setTextWrap(false);
+  tft.setTextSize(1.5);
+  tft.printf("L:%d", lowTemp);
+
+  tft.setCursor(highStartX, startY);
+  tft.printf("H:%d", highTemp);
+}
+
+void drawIcon(String type, int centerX, int centerY, int size) {
+  if (type == "sun") {
+    tft.fillCircle(centerX, centerY, size / 2, ST77XX_YELLOW);
+
+    // left
+    for (int i = 4; i <= 10; i += 2) {
+      tft.fillCircle(centerX - size / 2 - i, centerY, 3, ST77XX_YELLOW);
+    }
+
+    // top
+    for (int i = 4; i <= 10; i += 2) {
+      tft.fillCircle(centerX, centerY - size / 2 - i, 3, ST77XX_YELLOW);
+    }
+
+    // right
+    for (int i = 4; i <= 10; i += 2) {
+      tft.fillCircle(centerX + size / 2 + i, centerY, 3, ST77XX_YELLOW);
+    }
+
+    // bottom
+    for (int i = 4; i <= 10; i += 2) {
+      tft.fillCircle(centerX, centerY + size / 2 + i, 3, ST77XX_YELLOW);
+    }
+
+    // top right
+    int degrees = -45;
+    double angle_radians = degrees * M_PI / 180;
+    int x2 = centerX + (size / 2 + 4) * cos(angle_radians);
+    int y2 = centerY + (size / 2 + 4) * sin(angle_radians);
+    tft.fillCircle(x2, y2, 3, ST77XX_YELLOW);
+    tft.fillCircle(x2 + 2, y2 - 2, 3, ST77XX_YELLOW);
+
+    // bottom right
+    degrees = 45;
+    angle_radians = degrees * M_PI / 180;
+    x2 = centerX + (size / 2 + 4) * cos(angle_radians);
+    y2 = centerY + (size / 2 + 4) * sin(angle_radians);
+    tft.fillCircle(x2, y2, 3, ST77XX_YELLOW);
+    tft.fillCircle(x2 + 2, y2 + 2, 3, ST77XX_YELLOW);
+
+    // bottom left
+    degrees = 135;
+    angle_radians = degrees * M_PI / 180;
+    x2 = centerX + (size / 2 + 4) * cos(angle_radians);
+    y2 = centerY + (size / 2 + 4) * sin(angle_radians);
+    tft.fillCircle(x2, y2, 3, ST77XX_YELLOW);
+    tft.fillCircle(x2 - 2, y2 + 2, 3, ST77XX_YELLOW);
+
+    // top left
+    degrees = -135;
+    angle_radians = degrees * M_PI / 180;
+    x2 = centerX + (size / 2 + 4) * cos(angle_radians);
+    y2 = centerY + (size / 2 + 4) * sin(angle_radians);
+    tft.fillCircle(x2, y2, 3, ST77XX_YELLOW);
+    tft.fillCircle(x2 - 2, y2 - 2, 3, ST77XX_YELLOW);
+  } else if (type == "cloud") {
+    size *= 1.4;
+    tft.fillRoundRect(centerX - size / 16 * 11, centerY + size / 8, size + size / 8, size / 2, size / 4, COLOR_CLOUD);
+
+    // left
+    tft.fillCircle(centerX - size / 2, centerY + size / 4, size / 4, COLOR_CLOUD);
+
+    // top
+    tft.fillCircle(centerX - size / 4, centerY + size / 16, size / 3, COLOR_CLOUD);
+
+    // right
+    tft.fillCircle(centerX + size / 4, centerY + size / 6, size / 5 * 2, COLOR_CLOUD);
+  } else if (type == "rain") {
+    size *= 1.2;
+    tft.fillRoundRect(centerX - size / 16 * 11, centerY + size / 16, size + size / 8, size / 2, size / 4, COLOR_CLOUD);
+
+    // left
+    tft.fillCircle(centerX - size / 2, centerY + size / 4, size / 8, COLOR_CLOUD);
+
+    // top
+    tft.fillCircle(centerX - size / 4, centerY + size / 32, size / 3, COLOR_CLOUD);
+
+    // right
+    tft.fillCircle(centerX + size / 4, centerY + size / 12, size / 5 * 2, COLOR_CLOUD);
+
+    // rain
+    for (int i = 0; i <= 6; i += 2) {
+      tft.fillCircle(centerX - size / 3 + i, centerY + size / 2 * 1.3 + i, 2, COLOR_RAIN);
+      tft.fillCircle(centerX + i, centerY + size / 2 * 1.3 + i, 2, COLOR_RAIN);
+      tft.fillCircle(centerX + size / 3 + i, centerY + size / 2 * 1.3 + i, 2, COLOR_RAIN);
+    }
+  } else {
+    // cloud + sun
+  }
 }
